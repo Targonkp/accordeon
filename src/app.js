aad6afb706831c9c338d6fb8a09734c791062126
student <student@mail.ru>
2019-10-17 17:55:11 +0300

    Accordeon created

--- src/app.js
+++ src/app.js
@@ -1,41 +1,17 @@
 export default function () {
 
 
-    // function multiplication(...items) {
-    //     let sum = 1;
-    //     for (let i = 0; i < items.length; i++) {
-    //         sum *= items[i];
-    //     }
-    //     return sum;
-    // }
-    //
+let acc = document.getElementsByClassName('accordeon');
+let i;
 
-
-
-
-
-    const liEl = document.querySelectorAll('#ul li');
-    for (let i=0; i<liEl.length; i++){
-        liEl[i].addEventListener('click', function (e) {openbox(e)})
-    }
-
-
-    function openbox(e) {
-      if (e.target.classList.contains('on')){
-          // alert('Верно');
-          document.querySelector('div').classList.remove('box-open');
-          e.target.classList.remove('on');
-      }
-      else
-      {
-          // alert('Неверно');
-        document.querySelector('div').classList.add('box-open');
-          e.target.classList.add('on');
-
-      }
-      e.preventDefault();
-    }
-
-
+for(i=0; i<acc.length; i++){
+    acc[i].addEventListener(
+        'click',
+        function(){
+            this.classList.toggle('active');
+            this.nextElementSibling.classList.toggle('show');
+        }
+    )
+}
 
 }
